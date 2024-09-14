6bcd328f14e5a59d31a935347b9928f72504208a
Mohammed <bgd4500@gmail.com>
2024-09-14 09:26:22 +0300

    feature: get output work info

--- app/src/main/java/natour/developer/jumping_back_in/data/WorkerRepository.kt
+++ app/src/main/java/natour/developer/jumping_back_in/data/WorkerRepository.kt
@@ -3,16 +3,19 @@ package natour.developer.jumping_back_in.data
 import android.content.Context
 import android.net.Uri
 import android.util.Log
+import androidx.lifecycle.asFlow
 import androidx.work.Data
 import androidx.work.OneTimeWorkRequestBuilder
 import androidx.work.WorkInfo
 import androidx.work.WorkManager
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.mapNotNull
 import natour.developer.jumping_back_in.KEY_BLUR_LEVEL
 import natour.developer.jumping_back_in.KEY_IMAGE_URI
+import natour.developer.jumping_back_in.TAG_OUTPUT
 import natour.developer.jumping_back_in.worker.BlurWorker
 
-interface WorkerRepository{
+interface WorkerRepository {
     val outputWorkInfo: Flow<WorkInfo>
 
     fun applyBlur(imageUri: Uri, blurLevel: Int)
@@ -21,13 +24,15 @@ interface WorkerRepository{
 
 const val TAG = "repository"
 
-class WorkerRepositoryImpl(context: Context): WorkerRepository{
+class WorkerRepositoryImpl(context: Context) : WorkerRepository {
 
-//    private val imageUri: Uri = context.getImageUri()
+    //    private val imageUri: Uri = context.getImageUri()
     private val workManager = WorkManager.getInstance(context)
 
-    override val outputWorkInfo: Flow<WorkInfo>
-        get() = TODO("Not yet implemented")
+    override val outputWorkInfo: Flow<WorkInfo> =
+        workManager.getWorkInfosByTagLiveData(TAG_OUTPUT).asFlow().mapNotNull {
+            if (it.isNotEmpty()) it.first() else null
+        }
 
     override fun applyBlur(imageUri: Uri, blurLevel: Int) {
         val blurBuilder = OneTimeWorkRequestBuilder<BlurWorker>()
@@ -48,7 +53,7 @@ class WorkerRepositoryImpl(context: Context): WorkerRepository{
 }
 
 
-fun Context.getImageUri(): Uri{
-return Uri.EMPTY
+fun Context.getImageUri(): Uri {
+    return Uri.EMPTY
 
 }
